63be2817425fff7bb86ccb0ca3e4321dc3adfcae
amosmurmu <amosmurmu002@gmail.com>
2025-01-24 20:08:44 +0530

    fix:islogin updated

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -45,7 +45,7 @@ export const useAuthStore = create((set, get) => ({
   },
 
   login: async (data) => {
-    set({ isLogggingIn: true });
+    set({ isLoggingIn: true });
 
     try {
       const res = await axiosInstance.post("/auth/login", data);
