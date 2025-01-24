92e251aa83ba8d5a63d317b49c1d8fca74e373a9
amosmurmu <amosmurmu002@gmail.com>
2025-01-24 18:59:57 +0530

    fix:output.css

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
